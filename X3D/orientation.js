4afaae49bcd52c3d7d0db10d68e95d7b4ff55910
tedere93 <tedere87@gmail.com>
2018-03-16 18:36:40 +0200

    round

--- X3D/orientation.js
+++ X3D/orientation.js
@@ -1,5 +1,5 @@
 window.addEventListener("deviceorientation",ondeviceorientation);
 
 function ondeviceorientation (e){
-	document.getElementById("orientation_id").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
+	document.getElementById("orientation_id").innerHTML = e.alpha.round() + " " + e.beta.round() + " " + e.gamma.round();
 }
