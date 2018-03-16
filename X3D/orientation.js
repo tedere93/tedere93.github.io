f83dab521f382912877d8325429967133668de2a
tedere93 <tedere87@gmail.com>
2018-03-16 18:47:23 +0200

    upgrade

--- X3D/orientation.js
+++ X3D/orientation.js
@@ -2,4 +2,6 @@ window.addEventListener("deviceorientation",ondeviceorientation);
 
 function ondeviceorientation (e){
 	document.getElementById("orientation_id").innerHTML = e.alpha.round() + " " + e.beta.round() + " " + e.gamma;
+	var viewpoint = document.getElementById("id_viewpoint");
+	viewpoint.setAttribute("orientation","")
 }
