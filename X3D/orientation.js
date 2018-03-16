dc1f425bc76b3212be78333bd4f170bd4e1e81e4
tedere93 <tedere87@gmail.com>
2018-03-16 19:05:31 +0200

    jsssssssss

--- X3D/orientation.js
+++ X3D/orientation.js
@@ -2,7 +2,7 @@ window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e)
 {
-	document.getElementById("id_orientation").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+	document.getElementById("orientation_id").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
 	var viewpoint = document.getElementById("id_viewpoint");
 	viewpoint.setAttribute("orientation","0 0 1 " + e.alpha * Math.PI / 180);
 }
