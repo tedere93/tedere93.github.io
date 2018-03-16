ced0a2d20c94e6373982336afa237a91223f5089
tedere93 <tedere87@gmail.com>
2018-03-16 19:30:57 +0200

    Interschimbat valorile celor doua constante irationalae

--- X3D/orientareXZ.js
+++ X3D/orientareXZ.js
@@ -5,8 +5,8 @@ var radius = 20;
 function ondevor(e){
 	document.getElementById("orientation_id");
 	var viewpoint = document.getElementById("id_viewpoint");
-	var x = radius * Math.cos(e.alpha*Math.PI/180);
-	var z = radius * Math.sin(e.alpha*Math.PI/180);
+	var z = radius * Math.cos(e.alpha*Math.PI/180);
+	var x = radius * Math.sin(e.alpha*Math.PI/180);
 	viewpoint.setAttribute("position",x+" 0 "+ z);
 	viewpoint.setAttribute("orientation","0 1 0 "+e.alpha*Math.PI/180);
 }
