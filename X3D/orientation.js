8ddb4cd04a4d2c4c7201917d8a6eeb7c9e2d1bc8
tedere93 <tedere87@gmail.com>
2018-03-16 19:04:38 +0200

    new JS

--- X3D/orientation.js
+++ X3D/orientation.js
@@ -1,7 +1,8 @@
-window.addEventListener("deviceorientation",ondeviceorientation);
+window.addEventListener("deviceorientation", on_device_orientation);
 
-function ondeviceorientation (e){
-	document.getElementById("orientation_id").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
-// 	var viewpoint = document.getElementById("id_viewpoint");
-// 	viewpoint.setAttribute("orientation","0 1 1 "+e.alpha*Math.PI/180);
+function on_device_orientation(e)
+{
+	document.getElementById("id_orientation").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+	var viewpoint = document.getElementById("id_viewpoint");
+	viewpoint.setAttribute("orientation","0 0 1 " + e.alpha * Math.PI / 180);
 }
