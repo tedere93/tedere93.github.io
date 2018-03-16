ef6c030db12e051c4da9a1b2c9b4cd895666aebf
tedere93 <tedere87@gmail.com>
2018-03-16 18:52:13 +0200

    rotation

--- X3D/orientation.js
+++ X3D/orientation.js
@@ -3,5 +3,5 @@ window.addEventListener("deviceorientation",ondeviceorientation);
 function ondeviceorientation (e){
 	document.getElementById("orientation_id").innerHTML = e.alpha.round() + " " + e.beta.round() + " " + e.gamma;
 	var viewpoint = document.getElementById("id_viewpoint");
-	viewpoint.setAttribute("orientation","")
+	viewpoint.setAttribute("orientation","0 1 1 "+e.alpha*Math.PI/180);
 }
