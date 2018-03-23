32b32d2e8395da503fbd5e263e11ea136c5892cb
tedere93 <tedere87@gmail.com>
2018-03-23 18:11:56 +0200

    added BUSINESS version

--- eks3d/func.js
+++ eks3d/func.js
@@ -1,16 +1,16 @@
 
-	document.getElementById("business_V").innerHTML = "23/03/18 v1";
-	setInterval( deseneaza,30);
+	document.getElementById("business_V").innerHTML = "23/03/18 v2";
+	setInterval( deseneaza,15,0,1);
 	var y = 0;
-	var directie = 1;
-	function deseneaza(){
+	var directie = 0.1;
+	function deseneaza(y, directie){
 		var t = document.getElementById("id_transform");
 		t.setAttribute("translation","0 "+y+" 0");
 		y+=directie;
 		if(y>7){
-			directie = -1;
+			directie = -0.1;
 		} else if (y< -7){
-			directie = 1;
+			directie = 0.1;
 		}
 	 	
 
