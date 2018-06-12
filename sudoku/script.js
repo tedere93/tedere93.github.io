0b3b4e56e44c89be980c568bcf2b21f2dd2416aa
tedere93 <tedere87@gmail.com>
2018-06-12 20:23:17 +0200

    Added Stuff, Deleted Stuff

--- sudoku/script.js
+++ sudoku/script.js
@@ -1,12 +1,90 @@
 var canvas = document.getElementById('myCanv');
-console.log(canvas);
 context = canvas.getContext('2d');
 
-make_base();
+var canvas2 = document.getElementById('myCanv');
+ctx = canvas2.getContext('2d');
 
-function make_base()
-{
-  base_image = new Image();
-  base_image.src = 'sudoku.png';
-  context.drawImage(base_image, 100, 100);
+base_image = new Image();
+base_image.src = 'sudoku.png';
+base_image.onload = function(){
+	context.drawImage(base_image, 0, 0);
+	var image = context.getImageData(1,1,400,400);
+	var data = image.data;
+	for(var i = 0;i<data.length;i+=4){
+		var red = data[i];
+		var green = data[i+1];
+		var blue = data[i+2];
+
+		if(red>240&&green>240&&blue>240){
+			data[i] = 255;
+			data[i+1] = 255;
+			data[i+2] = 255;
+		} else {
+			data[i] = 0;
+			data[i+1] = 0;
+			data[i+2] = 0;
+		}
+	}
+	context.putImageData(image,0,0);
+
+	var diagonala = parseInt(Math.sqrt(canvas.height*canvas.height + canvas.width*canvas.width)+1);
+
+	var accumulator= new Array();
+
+	for(var i=0;i<diagonala;i++){
+		accumulator[i]= new Array(180);
+	}
+
+	for(var i=0;i<diagonala;i++){
+		for(var j=0;j<180;j++){
+			accumulator[i][j]=0;
+		}
+	}
+
+	for(var i=0;i<canvas.height;i++){
+		for(var j=0;j<canvas.width;j++){
+			var culoare = context.getImageData(i,j,1,1);
+			var data = culoare.data;
+			// console.log(data.length);
+			if(data[1]<255 && data[2]<255 && data[3]<255){
+				for(var theta=0;theta<180;theta++){
+					var r = parseInt((i-canvas.width/2)*Math.cos(theta/180/3.14)+(j - canvas.height/2)*Math.sin(theta/180/3.14));
+					accumulator[r+diagonala/2][theta]++;
+					// console.log(r+diagonala/2);
+				}
+			}
+
+		}
+	}
+
+
+	var maxacc = 0;
+	for(var i=0;i<diagonala;i++){
+		for(var j=0;j<180;j++){
+			if(maxacc<accumulator[i][j]){
+				maxacc = accumulator[i][j];
+			}
+		}
+	}
+
+
+
+	// scalez
+	for(var i=0;i<diagonala;i++){
+		for(var j=0;j<180;j++){
+			accumulator[i][j] = accumulator[i][j]/parseFloat(maxacc)*255;
+		}
+	}
+
+
+
+	// populez imaginea
+	for(var i=0;i<diagonala;i++){
+		for(var j=0;j<180;j++){
+			var vareabila = accumulator[i][j];
+			ctx.putImageData(vareabila,i,j);
+		}
+	}
+
+
 }
